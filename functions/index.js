27544c868fd452d1376e01dc37263b7ee5afea62
jack <actionjacks22@gmail.com>
2020-10-24 00:11:00 +0200

    fixed comments

--- functions/index.js
+++ functions/index.js
@@ -6,31 +6,31 @@ const stripe = require("stripe")(
 );
 //command for testing firebase backend
 //         firebase emulators:start
-// - App config
+//  App config
 const app = express();
 
-// - Middlewares
+//  Middlewares
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-// - API routes
-app.get("/", (request, response) => response.status(200).send("hello world"));
+//  API routes
+app.get("/", (request, response) => response.status(200).send("working"));
 
 app.post("/payments/create", async (request, response) => {
   const total = request.query.total;
 
-  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+  console.log("Payment", total);
 
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: total, // subunits of the currency
-    currency: "usd",
+    amount: total,
+    currency: "pln",
   });
 
-  // OK - Created
+  // cCreated
   response.status(201).send({
     clientSecret: paymentIntent.client_secret,
   });
 });
 
-// - Listen command
+// listen command
 exports.api = functions.https.onRequest(app);
